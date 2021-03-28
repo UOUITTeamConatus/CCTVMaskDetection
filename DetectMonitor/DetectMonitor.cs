687438bc6fd35cf512e8eb42b0acaafb5ddc04d0
swj1265 <swj1265@naver.com>
2021-03-28 20:16:28 +0900

    20210328

---
+++ DetectMonitor/DetectMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Keras.Models;
+using Keras.Applications.MobileNet;
+using OpenCvSharp.Dnn;
+
+namespace DetectMonitor
+{
+    public partial class DetectMonitor : Panel
+    {
+        private string currentPath = Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName);
+    }
+}
