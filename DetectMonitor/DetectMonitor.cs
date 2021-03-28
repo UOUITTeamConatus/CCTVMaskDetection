3bb0e842fe2e97a1382256d520ef018d3ea1d407
swj1265 <swj1265@naver.com>
2021-03-28 20:24:37 +0900

    20210328

--- DetectMonitor/DetectMonitor.cs
+++ DetectMonitor/DetectMonitor.cs
@@ -7,12 +7,31 @@ using System.Windows.Forms;
 using System.IO;
 using Keras.Models;
 using Keras.Applications.MobileNet;
+using Keras.PreProcessing.Image;
 using OpenCvSharp.Dnn;
+using Numpy;
 
 namespace DetectMonitor
 {
     public partial class DetectMonitor : Panel
     {
-        private string currentPath = Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName);
+        #region 1: 필드 값들
+        private static string currentPath = Path.GetDirectoryName(
+            Directory.GetParent(Environment.CurrentDirectory).Parent.FullName
+        );
+        private string prototxtPath = currentPath + "\\models\\deploy.prototxt";
+        private string caffemodelPath = currentPath + "\\models\\res10_300x300_ssd_iter_140000.caffemodel";
+        private string maskdetectorPath = currentPath + "\\models\\total.model";
+
+        private BaseModel model;
+        private Net facenet;
+        private MobileNetV2 mobilenetv2;
+        #endregion
+
+        public DetectMonitor()
+        {
+      
+        }
+
     }
 }
